d07ac8b707b1f7c3f4758f8e1d88ddc3ca2b95c9
BobdxProgrammer <116928333+BobdxProgrammer@users.noreply.github.com>
2024-02-29 16:13:53 -0500

    Create get.java

---
+++ get.java
@@ -0,0 +1,9 @@
+public getRange3d(){
+  double range =
+                        PhotonUtils.calculateDistanceToTargetMeters(
+                                CAMERA_HEIGHT_METERS,
+                                TARGET_HEIGHT_METERS,
+                                CAMERA_PITCH_RADIANS,
+                                Units.degreesToRadians(result.getBestTarget().getPitch()));
+
+}
